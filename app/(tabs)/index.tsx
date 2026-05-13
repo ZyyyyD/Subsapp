063b9a3823b0d5d4219fe2eadabdbbbd41721db4
zyyyd <zyrelfrias24@gmail.com>
2026-05-13 23:30:47 +0800

    Feat: project setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,14 @@
 import "@/global.css";
+
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <SafeAreaView className="flex-1 bg-background p-5">
       <Text className="text-xl font-bold text-blue-500">
         Welcome to Nativewind!
       </Text>
@@ -34,6 +38,6 @@ export default function App() {
         className="mt-4 rounded bg-black text-white p-4">
         Claude Subscription
       </Link>
-    </View>
+    </SafeAreaView>
   );
 }
--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,1 +1,12 @@
-export default function Settings() {}
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSafeAreaView);
+
+export default function Settings() {
+  return (
+    <SafeAreaView className="flex-1 bg-background p-5">
+      <Text className="text-xl font-bold text-blue-500">Settings</Text>
+    </SafeAreaView>
+  );
+}
